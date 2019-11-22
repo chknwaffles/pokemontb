610ff8606d58900612de2366b754fa622515d09c
chknwaffles <wonsae.kim@gmail.com>
2019-11-21 19:41:10 -0500

    teamcard stuff

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Card, Col, Row } from 'antd';
 
+const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+};
+
 export default function TeamCard(props) {
     const { team } = props
 
     const renderPokemon = () => {
-        return team.pokemon.forEach(poke => {
-            return <Card>
-               
+        if (team.pokemons == null) {
+            return <Card type='inner' hoverable={true} onClick={() => addPokemon()}>
+                Add a pokemon!
             </Card>
-        })
+        } else {
+            return team.pokemons.forEach(poke => {
+                return <Card.Grid style={gridStyle}>
+                    {poke.name}
+                </Card.Grid>
+            })
+        }
+        
+    }
+
+    const addPokemon = () => {
+
     }
 
     return (
         <div className={`team-card-${team.id}`} style={{ background: '#ECECEC', padding: '30px' }}>
             <Row gutter={16}>
                 <Col span={8}>
-                    <Card title="Card title" bordered={false}>
-                        Card content
+                    <Card title={team.name} bordered={false} >
+                        {renderPokemon()}
                     </Card>
                 </Col>
             </Row>
