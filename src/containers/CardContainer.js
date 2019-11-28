c2c14955055db37e31f9f4381cc337d87c30060c
chknwaffles <wonsae.kim@gmail.com>
2019-11-27 22:48:29 -0500

    refactor components and file structure

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, Descriptions, Tag, Button } from 'antd'
+import { Card, Descriptions, Tag, Button, Modal } from 'antd'
 import "antd/dist/antd.css";
 
 const imageStyle = {
@@ -9,9 +9,10 @@ const imageStyle = {
     cursor: 'pointer'
 }
 
-export default function PokemonCard(props) {
-    const { pokedex_entry, name, height, weight, types, sprites, handlePokemonDetails } = props
+export default function CardContainer(props) {
+    const { pokedex_entry, name, height, weight, types, sprites, handlePokemonDetails, teams } = props
     const [image, setImage] = useState(sprites[0])
+    const [modal, showModal] = useState(false)
 
     const handleSprite = () => {
         if (image === sprites[0])
@@ -21,6 +22,7 @@ export default function PokemonCard(props) {
     }
 
     const formatName = () => `#${pokedex_entry} ` + name.charAt(0).toUpperCase() + name.slice(1, name.length)
+    
     const getColorFromType = (typeName) => {
         switch(typeName) {
             case 'poison': return '#A040A0'
@@ -53,19 +55,34 @@ export default function PokemonCard(props) {
         })
     }
 
-    return (
-        <Card
-            style={{ width: '225px', height: '280px' }}
-            cover={<img style={imageStyle} alt={name} src={image} onClick={() => handleSprite()}/>}
-        > 
-            <Descriptions title={formatName()} border size='small'>
-                <Descriptions.Item label='Type'> {renderTypeBadges()} </Descriptions.Item>
-                <Descriptions.Item label='Height'> {height} </Descriptions.Item>
-                <Descriptions.Item label='Weight'> {weight} </Descriptions.Item>  
-            </Descriptions>
+    const showTeams = () => {
+        // loop to return all teams on each card
+    }
 
-            <Button type="primary" onClick={() => handlePokemonDetails(props)}>Details</Button>
-            <Button icon="plus-circle" />
-        </Card>
+    return (
+        <div className='card-container'>
+            <Card
+                style={{ width: '225px', height: '280px' }}
+                cover={<img style={imageStyle} alt={name} src={image} onClick={() => handleSprite()}/>}
+            > 
+                <Descriptions title={formatName()} border size='small'>
+                    <Descriptions.Item label='Type'> {renderTypeBadges()} </Descriptions.Item>
+                    <Descriptions.Item label='Height'> {height} </Descriptions.Item>
+                    <Descriptions.Item label='Weight'> {weight} </Descriptions.Item>  
+                </Descriptions>
+
+                <Button type="primary" onClick={() => handlePokemonDetails(props)}>Details</Button>
+                <Button icon="plus-circle" onClick={() => showModal(true)} />
+            </Card>
+            <Modal
+            title='Which team would you like to add this pokemon to?'
+            visible={modal}
+            onOk={addTeam}
+            onCancel={handleCancel}
+            >
+                {showTeams()}
+            </Modal>
+
+        </div>
     )
 }
--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { List } from 'antd'
-import PokemonCard from '../components/PokemonCard'
+import CardContainer from './CardContainer'
 
 export default function PokemonContainer(props) {
-    const { allPokemon, setPokemon } = props
+    const { allPokemon, setPokemon, teams } = props
     const history = useHistory()
 
     const handlePokemonDetails = (pokeObj) => {
@@ -29,7 +29,7 @@ export default function PokemonContainer(props) {
                 dataSource={allPokemon}
                 renderItem={item => (
                     <List.Item key={item.id} >
-                        <PokemonCard {...item} handlePokemonDetails={handlePokemonDetails} />
+                        <CardContainer {...item} handlePokemonDetails={handlePokemonDetails} teams={teams} />
                     </List.Item>
                 )}
             />
